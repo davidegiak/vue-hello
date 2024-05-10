2b82c3e79c0c7a6b67c8d536e1d5187850c99295
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-10 16:29:56 +0200

    fine + bonus

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const { createApp, ref } = Vue
+const { createApp } = Vue
 
 createApp({
     data() {
         return {
             message:"Hello",
-            user:"ciao"
+            user:"ciao",
+            img: "spider-man-across-3840x2160-11476.png"
         }
     }
 }).mount('#app')
